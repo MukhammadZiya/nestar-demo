8e759522679afca2877ccb4d8db52c7d0744a15f
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-26 23:33:58 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
+import { light } from "@/scss/MaterialTheme";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  //@ts-ignore
+
+  const [theme, setTheme] = useState(createTheme(light));
+
+  //Socket.io, redux, mui
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,14 +10,11 @@ export default function Document() {
         <meta name="title" content="Nestar Demo" />
         <meta name="robots" content="index,follow" />
         <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
-
-
-        // SEO
+        /**SEO */
         <meta
           name="keyword"
           content={"nestar, nestar.uz, devex, mern, mern nestjs fullstack"}
         />
-
         <meta
           name="description"
           content={"Buy and sell properties anywhere anytime in South Korea"}
