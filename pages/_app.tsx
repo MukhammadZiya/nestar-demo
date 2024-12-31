924afb2a8a227259b0c0b65c3a6fe9926c5a7ccd
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-31 23:57:56 +0900

    feat: publish basic layout components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "../scss/app.scss";
-import "../scss/pc/main.scss";
+import "../scss/pc/property/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
