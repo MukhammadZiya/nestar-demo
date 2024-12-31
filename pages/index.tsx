1748f2745e1e5fa9eef24b1cda82e4d1e1af9b0d
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-31 23:11:31 +0900

    feat: publish layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,19 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack>
-        {" "}
-        <Stack flexDirection={"column"}>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack flexDirection={"column"}>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack flexDirection={"column"}>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
-        <Stack flexDirection={"column"}>
-          <Stack className="container">Events</Stack>
-        </Stack>
-      </Stack>
+    <Stack className={"home-page"}>
+          <Stack>
+            <Stack className="container">Popular Properties</Stack>
+          </Stack>
+          <Stack>
+            <Stack className="container">Top Agents</Stack>
+          </Stack>
+          <Stack>
+            <Stack className="container">Top Properties</Stack>
+          </Stack>
     </Stack>
   );
 };
