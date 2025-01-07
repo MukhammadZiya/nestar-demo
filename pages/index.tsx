e4d0e0f250bfeb5228c7d3ac621e387f40756211
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-07 10:03:11 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,31 @@ import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTY } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTY, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData => ", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
